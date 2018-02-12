d05d90b04ce0ff7cd878279eedd0d87bf606829d
Michael Pumo <hello@michaelpumo.com>
2018-02-12 16:48:10 +0000

    404 Page.

--- src/pages/PotatoError.vue
+++ src/pages/PotatoError.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="Error">
-    <h1>{{ title }}</h1>
+    <div class="Error__main">
+      <h1>Page not found 🤔</h1>
+      <p><router-link :to="{ name: 'PotatoFeed' }">Get me outta here!</router-link></p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PotatoError',
-  data () {
-    return {
-      title: 'Error'
-    }
-  }
+  name: 'PotatoError'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../assets/scss/base/variables.scss";
+@import "../assets/scss/base/placeholders.scss";
+@import "../assets/scss/base/mixins.scss";
+
 .Error {
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: calc(100vh - #{$header-height});
   
+  &__main {
+
+    max-width: $max-site-width;
+    text-align: center;
+
+  }
+
 }
 </style>
